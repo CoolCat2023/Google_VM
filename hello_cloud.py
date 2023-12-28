b7129564a5de2792feb3b3b60dba5d8e106c6ab6
Nisha <nisha@example.com>
2023-12-28 14:51:26 +0000

    Add a simple python script in hello_cloud.py

--- hello_cloud.py
+++ hello_cloud.py
@@ -1,1 +1,22 @@
-Writing a simple script to run hello_cloud.py file
+#!/usr/bin/env python3
+
+#Writing a simple script to run hello_cloud.py file
+
+
+#importing socket module
+
+import socket 
+
+#getting the hostname by socket .gethostname() method
+
+hostname=socket.gethostname()
+
+#getting the ip address using socket.gethostname() method
+
+ip_address=socket.gethostbyname(hostname)
+
+#printing the hostname and ip_address 
+print("Hello Cloud")
+print(f'Hostname: {hostname}')
+print(f'IP_address: {ip_address}')
+
